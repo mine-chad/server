7f4d0180cb5a23da1e6c2e70e3933957af8bb3f8
Pixel <pixel@eloteam.tk>
2023-06-04 13:59:28 +0200

    Added copyright comments, as mentioned in #5

--- src/logging/color.cpp
+++ src/logging/color.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2023 mine-chad
+ * This file is part of the Mine-chad project.
+ *
+ * Mine-chad is free software: you can redistribute it and/or modify it under
+ * the terms of the MIT License.
+ *
+ * Mine-chad is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the MIT License for more details.
+ *
+ * You should have received a copy of the MIT License along with Mine-chad. If
+ * not, see <https://mit-license.org/> or contact the Massachusetts Institute
+ * of Technology (MIT) at <tlo-inquiries@mit.edu>.
+ */
+
 #include "color.h"
 
 std::ostream &color::operator<<(std::ostream &os, const color::RGB &rgb) {
