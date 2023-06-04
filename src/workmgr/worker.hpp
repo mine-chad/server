27c0cff678b38ce6b15b1249bd99ac92f2a54037
TheRealOne78 <bajcsielias78@gmail.com>
2023-06-04 22:13:19 +0300

    Add copyright comments to d84cb9b

--- src/workmgr/worker.hpp
+++ src/workmgr/worker.hpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2023 mine-chad
+ * This file is part of the Mine-chad project.
+ *
+ * Mine-chad is free software: you can redistribute it and/or modify it under
+ * the terms of the MIT License.
+ *
+ * Mine-chad is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the MIT License for more details.
+ *
+ * You should have received a copy of the MIT License along with Mine-chad. If
+ * not, see <https://mit-license.org/> or contact the Massachusetts Institute
+ * of Technology (MIT) at <tlo-inquiries@mit.edu>.
+ */
+
 #pragma once
 
 #include <sys/wait.h>
@@ -42,7 +58,7 @@ public:
 	{
 		if (active()) throw std::runtime_error("Worker is busy.");
 		data = WorkerData();
-		
+
 		func = payload;
 		th = std::thread(func, &data, arg);
 		func = 0;
--- src/workmgr/workmgr.hpp
+++ src/workmgr/workmgr.hpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2023 mine-chad
+ * This file is part of the Mine-chad project.
+ *
+ * Mine-chad is free software: you can redistribute it and/or modify it under
+ * the terms of the MIT License.
+ *
+ * Mine-chad is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the MIT License for more details.
+ *
+ * You should have received a copy of the MIT License along with Mine-chad. If
+ * not, see <https://mit-license.org/> or contact the Massachusetts Institute
+ * of Technology (MIT) at <tlo-inquiries@mit.edu>.
+ */
+
 #pragma once
 
 #include "worker.hpp"
@@ -41,7 +57,7 @@ public:
 			std::this_thread::sleep_for(std::chrono::nanoseconds(1000));
 			}
 		}
-		
+
 		// Wait till all workers will have their jobs finished
 		for (auto &j : workers) {
 			j.join();
